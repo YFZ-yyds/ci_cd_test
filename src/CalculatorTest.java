be8f63e2f9461d7e6e457c58d2994397e2f56443
wind-c-language <m15979615403@163.com>
2026-04-15 19:45:43 +0800

    第一次提交项目

---
+++ src/CalculatorTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    private Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, calc.subtract(3, 2));
+    }
+}
